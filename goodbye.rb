a6a3b7e041b7822a6bf3c720d26cb53f22d5248e
mattdadabo <mdadabo@chicagobooth.edu>
2024-07-06 19:53:48 +0000

    Initial Commit

---
+++ goodbye.rb
@@ -0,0 +1,3 @@
+my_string = "see ya later!"
+
+pp my_string
---
+++ howdy.rb
@@ -0,0 +1,8 @@
+require "./goodbye.rb"
+require "active_support/all"
+
+  #my_string = "Howdy there!"
+
+  #pp my_string
+
+pp 4.to_s + "th"
